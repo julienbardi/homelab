926c100925d7684e4ff54bbfd75f0833a8fd4053
Julien Bardi <25685298+julienbardi@users.noreply.github.com>
2025-11-08 13:29:31 +0100

    We need to quote every PostUp and PostDown command string to tell the parser to treat the entire command as a single value.

--- 10.89.12.4/usr/local/bin/wg.sh
+++ 10.89.12.4/usr/local/bin/wg.sh
@@ -375,28 +375,30 @@ local wg_ipv6_subnet="fd10:$num::/64"
 
 cat <<EOF
 # --- START ROUTING & NAT RULES ---
-PostUp = sysctl -w net.ipv4.ip_forward=1
-PostUp = sysctl -w net.ipv6.conf.all.forwarding=1
+# CRITICAL FIX: Quote all commands to prevent the parser from confusing '=' within 
+# the shell command with a parameter assignment (e.g., PostUp = "command...").
+PostUp = "sysctl -w net.ipv4.ip_forward=1"
+PostUp = "sysctl -w net.ipv6.conf.all.forwarding=1"
 # IPv4 FORWARD chain
-PostUp = iptables-legacy -A FORWARD -i %i -j ACCEPT
-PostUp = iptables-legacy -A FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+PostUp = "iptables-legacy -A FORWARD -i %i -j ACCEPT"
+PostUp = "iptables-legacy -A FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
 # IPv4 NAT (MASQUERADE)
 # Rule: NAT traffic from WG subnet to anywhere *except* the LAN.
 # This relies on your router's static route for WG->LAN traffic.
-PostUp = iptables-legacy -t nat -A POSTROUTING -s $wg_ipv4_subnet -o $NAS_LAN_IFACE ! -d $LAN_SUBNET -j MASQUERADE
+PostUp = "iptables-legacy -t nat -A POSTROUTING -s $wg_ipv4_subnet -o $NAS_LAN_IFACE ! -d $LAN_SUBNET -j MASQUERADE"
 # IPv6 FORWARD chain
-PostUp = ip6tables-legacy -A FORWARD -i %i -j ACCEPT
-PostUp = ip6tables-legacy -A FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+PostUp = "ip6tables-legacy -A FORWARD -i %i -j ACCEPT"
+PostUp = "ip6tables-legacy -A FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
 # IPv6 NAT (MASQUERADE)
 # Note: This NATs all IPv6.
-PostUp = ip6tables-legacy -t nat -A POSTROUTING -s $wg_ipv6_subnet -o $NAS_LAN_IFACE -j MASQUERADE
+PostUp = "ip6tables-legacy -t nat -A POSTROUTING -s $wg_ipv6_subnet -o $NAS_LAN_IFACE -j MASQUERADE"
 # --- CLEANUP RULES ---
-PostDown = iptables-legacy -D FORWARD -i %i -j ACCEPT
-PostDown = iptables-legacy -D FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-PostDown = iptables-legacy -t nat -D POSTROUTING -s $wg_ipv4_subnet -o $NAS_LAN_IFACE ! -d $LAN_SUBNET -j MASQUERADE
-PostDown = ip6tables-legacy -D FORWARD -i %i -j ACCEPT
-PostDown = ip6tables-legacy -D FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-PostDown = ip6tables-legacy -t nat -D POSTROUTING -s $wg_ipv6_subnet -o $NAS_LAN_IFACE -j MASQUERADE
+PostDown = "iptables-legacy -D FORWARD -i %i -j ACCEPT"
+PostDown = "iptables-legacy -D FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
+PostDown = "iptables-legacy -t nat -D POSTROUTING -s $wg_ipv4_subnet -o $NAS_LAN_IFACE ! -d $LAN_SUBNET -j MASQUERADE"
+PostDown = "ip6tables-legacy -D FORWARD -i %i -j ACCEPT"
+PostDown = "ip6tables-legacy -D FORWARD -o %i -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
+PostDown = "ip6tables-legacy -t nat -D POSTROUTING -s $wg_ipv6_subnet -o $NAS_LAN_IFACE -j MASQUERADE"
 # --- END ROUTING & NAT RULES ---
 EOF
 }
