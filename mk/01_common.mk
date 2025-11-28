4faae4236b6761d5e37c1cdde8abf4fda742d72a
Jambo15 <Jambo15@users.noreply.github.com>
2025-11-28 18:58:24 +0100

    headscale cleanup

--- mk/01_common.mk
+++ mk/01_common.mk
@@ -15,9 +15,20 @@ endef
 
 
 # run_as_root(command). Privilege guard with id -u (0 if root) evaluated at runtime not at parse time ---
-#run_as_root = bash -c "if [ \"\$(id -u)\" -eq 0 ]; then eval \"\$1\"; else sudo bash -c \"\$1\"; fi" -- $(1)
-#run_as_root = bash -c 'if [ "$$(id -u)" -eq 0 ]; then exec "$@"; else exec sudo bash -c '"'"'"$@"'"'"'; fi' -- $(1)
-run_as_root = bash -c 'if [ "$$(id -u)" -eq 0 ]; then bash -c "$$1"; else sudo bash -c "$$1"; fi' -- '$(1)'
+
+#run_as_root = bash -c "if [ \"\$(id -u)\" -eq 0 ]; then eval \"\$1\"; else sudo bash -c \"\$1\"; fi" -- $(1) #wrong
+#run_as_root = bash -c 'if [ "$$(id -u)" -eq 0 ]; then exec "$@"; else exec sudo bash -c '"'"'"$@"'"'"'; fi' -- $(1) #wrong
+
+#run_as_root = bash -c 'if [ "$$(id -u)" -eq 0 ]; then bash -c "$$1"; else sudo bash -c "$$1"; fi' -- '$(1)' #ok but noisy
+# run_as_root: execute command as root, log to journald
+run_as_root = bash -c '\
+	if [ "$$(id -u)" -eq 0 ]; then \
+		logger -t homelab-run_as_root "[root] $$1"; \
+		bash -c "$$1"; \
+	else \
+		logger -t homelab-run_as_root "[sudo] $$1"; \
+		sudo bash -c "$$1"; \
+	fi' -- '$(1)'
 
 # install_script(src, name)
 define install_script
